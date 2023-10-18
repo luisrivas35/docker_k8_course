a123f65b0d217c21d03508a6a4e7a8f0c0ba3535
Luis Rivas <xluisrivas@gmail.com>
2023-10-18 16:01:09 -0300

    update main

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from flask import Flask
-#from redis import Redis
+from redis import Redis
 
 app = Flask(__name__)
-#redis = Redis(host='redis', port=6379)
+redis = Redis(host='redis', port=6379)
 
 
 @app.route('/')
 def hello_world():
-    #redis.incr('hits')
-    return 'KEEP LEARNING, KEEP MOVING AHEAD Page_Count %s'
-    #% redis.get('hits')
+    redis.incr('hits')
+    return 'KEEP LEARNING, KEEP MOVING AHEAD Page_Count %s' % redis.get('hits')
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
-    #app.run(port=5001, debug=True)
+    # app.run(port=5001, debug=True)
+    

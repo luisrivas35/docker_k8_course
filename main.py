3e19f9410188d828c03b2808ce694f8d0fa12b5f
Luis Rivas <xluisrivas@gmail.com>
2023-10-18 20:54:15 -0300

    fix code

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
-
+redis = Redis(host='redis-service', port=6379)
 
 @app.route('/')
 def hello_world():
     redis.incr('hits')
-    return 'KEEP LEARNING, KEEP MOVING AHEAD Page_Count %s' % redis.get('hits')
-
+    page_count = redis.get('hits').decode('utf-8')  # Convert bytes to string
+    return f'KEEP LEARNING, KEEP MOVING AHEAD Page_Count {page_count}'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
-    # app.run(port=5001, debug=True)
-    
